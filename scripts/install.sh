d7773b8697a7fc3aba49126c256bc3cbd60ad4dd
Luca Della Libera <luca.dellalib@gmail.com>
2023-09-17 14:30:11 -0400

    Update

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,7 @@
+# Activate the virtual environment: conda activate pytorch_p38
+
 mkdir workspace
 cp -r /efs-storage/TS-ASR workspace/
 cd workspace/TS-ASR
-conda activate pytorch_p38
 pip install -e vendor/speechbrain
 pip install -r requirements.txt
